e7f3651a9898e937f9483c6f4edeab56f8b3b779
adamtlee <adam.lee346@gmail.com>
2022-05-15 14:25:10 -0600

    added read method for csv - refactored project structure

--- Brackets.Console/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Brackets Tool.");
-        Console.ReadKey();
-    }
-}
-
---
+++ src/Brackets.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Brackets.ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Brackets Tool.");
+
+            string path = "C:/Brackets/athletes.csv";
+            string[] lines = File.ReadAllLines(path);
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(',');
+                foreach (string part in parts)
+                {
+                    Console.WriteLine(part);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
