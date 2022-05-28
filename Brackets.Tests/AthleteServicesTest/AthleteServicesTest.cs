81490689423f4a451c020a932c5e487a89655b63
adamtlee <adam.lee346@gmail.com>
2022-05-27 18:03:31 -0600

    Added Tests

---
+++ Brackets.Tests/AthleteServicesTest/AthleteServicesTest.cs
@@ -0,0 +1,53 @@
+﻿using Brackets.Services.AthleteService;
+using NUnit.Framework;
+using System;
+
+namespace Brackets.Tests.AthleteServicesTest
+{
+    public class AthleteServicesTest
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CalculateTotalMatches_Should_Pass()
+        {
+            // Arrange
+            AthleteServices athleteServices = new AthleteServices(); 
+
+            Random randomIntGenerator = new Random();
+            var wins = randomIntGenerator.Next();
+            var losses = randomIntGenerator.Next();
+            var draws = randomIntGenerator.Next();
+            var total = wins + losses + draws;
+
+            // Act
+            var result = athleteServices.CalculateTotalMatches(wins, losses, draws);
+
+            // Assert
+            Assert.AreEqual(total, result);
+            
+        }
+
+        [Test]
+        public void CalculateTotalMatches_Should_Fail()
+        {
+            // Arrange
+            AthleteServices athleteServices = new AthleteServices();
+          
+            var wins = 5;
+            var losses = 5;
+            var draws = 5;
+            var total = wins + losses + draws + 1;
+
+            // Act
+            var result = athleteServices.CalculateTotalMatches(wins, losses, draws);
+
+            // Assert
+            Assert.AreNotEqual(total, result);
+        }
+    }
+}
